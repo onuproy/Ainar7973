3803d7f3d70b32c57b6b84b31e5a349d8ce7ab40
Onup Chandra Barmon <anupme01@gmail.com>
2022-03-10 13:09:49 +0600

    last update

---
+++ assets/js/custom.js
@@ -0,0 +1,14 @@
+jQuery(document).ready(function(){ 
+
+	// Mobile menu
+	$('.nav-icon').click(function () {
+		$('.mobile_menu').toggleClass('canvas-menu');
+		return false;
+
+	});
+
+		
+		
+	
+
+});
